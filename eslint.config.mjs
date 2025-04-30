3f6cf3d71d39b06d99929e448f1d9bbce0814c46
Sumit Jha <sumitjha.2266@gmail.com>
2025-05-01 01:08:33 +0530

    bug fix 5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginTs from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "@typescript-eslint": pluginTs,
+    },
+    rules: {
+      "@typescript-eslint/ban-types": "off", // Disable ban-types globally
+    },
+  },
 ];
 
 export default eslintConfig;
